1ddfddf65d70287ea0820ddd2de36a12d5c851b7
edwardlee03 <bert825_work@163.com>
2022-03-19 23:26:26 +0800

    improve the code and comment of JodaTimeAssertionsExamples

--- src/test/java/org/assertj/examples/JodaTimeAssertionsExamples.java
+++ src/test/java/org/assertj/examples/JodaTimeAssertionsExamples.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 
 /**
  * Joda Time assertions example.
+ * Joda Time的断言示例。
  * 
  * @author Joel Costigliola
  */
@@ -31,35 +32,54 @@ public class JodaTimeAssertionsExamples extends AbstractAssertionsExamples {
   @Test
   public void dateTime_assertions_examples() {
 
-    assertThat(new DateTime("2000-01-01")).isEqualTo(new DateTime("2000-01-01")).isNotEqualTo(
-        new DateTime("2000-01-15"));
+    assertThat(new DateTime("2000-01-01"))
+        .isEqualTo(new DateTime("2000-01-01"))
+        .isNotEqualTo(new DateTime("2000-01-15"));
     // same assertions but parameters is String based representation of DateTime
-    assertThat(new DateTime("2000-01-01")).isEqualTo("2000-01-01").isNotEqualTo("2000-01-15");
+    assertThat(new DateTime("2000-01-01"))
+        .isEqualTo("2000-01-01")
+        .isNotEqualTo("2000-01-15");
 
-    assertThat(new DateTime("2000-01-01")).isBeforeOrEqualTo(new DateTime("2000-01-01"));
-    assertThat(new DateTime("2000-01-01")).isAfterOrEqualTo(new DateTime("2000-01-01"));
-    assertThat(new DateTime("2000-01-01")).isBefore(new DateTime("2000-01-02")).isAfter(new DateTime("1999-12-31"));
+    assertThat(new DateTime("2000-01-01"))
+        .isBeforeOrEqualTo(new DateTime("2000-01-01"));
+    assertThat(new DateTime("2000-01-01"))
+        .isAfterOrEqualTo(new DateTime("2000-01-01"));
+    assertThat(new DateTime("2000-01-01"))
+        .isBefore(new DateTime("2000-01-02"))
+        .isAfter(new DateTime("1999-12-31"));
     // same assertions but parameters is String based representation of DateTime
-    assertThat(new DateTime("2000-01-01")).isBefore("2000-01-02").isAfter("1999-12-31");
+    assertThat(new DateTime("2000-01-01"))
+        .isBefore("2000-01-02")
+        .isAfter("1999-12-31");
 
-    assertThat(new DateTime("2000-01-01")).isIn(new DateTime("1999-12-31"), new DateTime("2000-01-01"));
-    assertThat(new DateTime("2000-01-01")).isNotIn(new DateTime("1999-12-31"), new DateTime("2000-01-02"));
+    assertThat(new DateTime("2000-01-01"))
+        .isIn(new DateTime("1999-12-31"), new DateTime("2000-01-01"));
+    assertThat(new DateTime("2000-01-01"))
+        .isNotIn(new DateTime("1999-12-31"), new DateTime("2000-01-02"));
     // same assertions but parameters is String based representation of DateTime
-    assertThat(new DateTime("2000-01-01")).isIn("1999-12-31", "2000-01-01").isNotIn("1999-12-31", "2000-01-02");
+    assertThat(new DateTime("2000-01-01"))
+        .isIn("1999-12-31", "2000-01-01")
+        .isNotIn("1999-12-31", "2000-01-02");
   }
 
   @Test
   public void localDateTime_assertions_examples() {
 
-    assertThat(new LocalDateTime("2000-01-01")).isEqualTo(new LocalDateTime("2000-01-01")).isNotEqualTo(
-        new LocalDateTime("2000-01-15"));
+    assertThat(new LocalDateTime("2000-01-01"))
+        .isEqualTo(new LocalDateTime("2000-01-01"))
+        .isNotEqualTo(new LocalDateTime("2000-01-15"));
     // same assertions but parameters is String based representation of LocalDateTime
-    assertThat(new LocalDateTime("2000-01-01")).isEqualTo("2000-01-01").isNotEqualTo("2000-01-15");
+    assertThat(new LocalDateTime("2000-01-01"))
+        .isEqualTo("2000-01-01")
+        .isNotEqualTo("2000-01-15");
 
-    assertThat(new LocalDateTime("2000-01-01")).isBefore(new LocalDateTime("2000-01-02")).isAfter(
-        new LocalDateTime("1999-12-31"));
+    assertThat(new LocalDateTime("2000-01-01"))
+        .isBefore(new LocalDateTime("2000-01-02"))
+        .isAfter(new LocalDateTime("1999-12-31"));
     // same assertions but parameters is String based representation of LocalDateTime
-    assertThat(new LocalDateTime("2000-01-01")).isBefore("2000-01-02").isAfter("1999-12-31");
+    assertThat(new LocalDateTime("2000-01-01"))
+        .isBefore("2000-01-02")
+        .isAfter("1999-12-31");
 
     assertThat(new LocalDateTime("2000-01-01")).isIn(new LocalDateTime("1999-12-31"), new LocalDateTime("2000-01-01"));
     assertThat(new LocalDateTime("2000-01-01")).isNotIn(new LocalDateTime("1999-12-31"),
@@ -72,10 +92,12 @@ public class JodaTimeAssertionsExamples extends AbstractAssertionsExamples {
   public void mixing_core_and_joda_time_assertions_examples() {
 
     // assertThat comes from org.assertj.jodatime.api.JODA_TIME.assertThat static import
-    assertThat(new DateTime("2000-01-01")).isEqualTo(new DateTime("2000-01-01"));
+    assertThat(new DateTime("2000-01-01"))
+        .isEqualTo(new DateTime("2000-01-01"));
 
     // assertThat comes from org.assertj.core.api.Assertions.assertThat static import
-    assertThat("hello world").startsWith("hello");
+    assertThat("hello world")
+        .startsWith("hello");
 
     // let's see if ShouldBeAfter error
     try {
