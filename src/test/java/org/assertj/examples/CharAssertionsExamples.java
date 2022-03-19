8eace166e2b1c61aad37f2b668411edb92d8e172
edwardlee03 <bert825_work@163.com>
2022-03-19 23:08:39 +0800

    improve the code and comment of CharAssertionsExamples

--- src/test/java/org/assertj/examples/CharAssertionsExamples.java
+++ src/test/java/org/assertj/examples/CharAssertionsExamples.java
@@ -18,30 +18,38 @@ import org.junit.Test;
 
 /**
  * char assertions examples.<br>
+ * 字符的断言示例。
  * 
  * @author Joel Costigliola
  */
 public class CharAssertionsExamples extends AbstractAssertionsExamples {
 
   @Test
-  public void char_assertions_examples() throws Exception {
+  public void char_assertions_examples() {
 
     // equals / no equals assertions
-    assertThat('a').isNotEqualTo('A');
+    assertThat('a')
+        .isNotEqualTo('A');
 
     // <= < > >= assertions
-    assertThat('a').isGreaterThan('A').isGreaterThanOrEqualTo('a');
-    assertThat('A').isLessThan('a').isLessThanOrEqualTo('a');
+    assertThat('a')
+        .isGreaterThan('A').isGreaterThanOrEqualTo('a');
+    assertThat('A')
+        .isLessThan('a').isLessThanOrEqualTo('a');
 
     // case assertions
-    assertThat('A').isUpperCase();
-    assertThat('a').isLowerCase();
+    assertThat('A')
+        .isUpperCase();
+    assertThat('a')
+        .isLowerCase();
   }
 
   @Test
   public void char_assertions_with_custom_comparison_examples() {
-    assertThat('a').usingComparator(caseInsensitiveComparator).isEqualTo('A');
-    assertThat(new Character('a')).usingComparator(caseInsensitiveComparator).isEqualTo(new Character('A'));
+    assertThat('a').usingComparator(caseInsensitiveComparator)
+        .isEqualTo('A');
+    assertThat(new Character('a')).usingComparator(caseInsensitiveComparator)
+        .isEqualTo(new Character('A'));
   }
 
 }
