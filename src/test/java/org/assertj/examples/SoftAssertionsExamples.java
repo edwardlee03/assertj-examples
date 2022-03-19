21cc9440580e90740f9e9a241285228bf2b2b0b9
edwardlee03 <bert825_work@163.com>
2022-03-19 23:34:58 +0800

    improve the code and comment of SoftAssertionsExamples

--- src/test/java/org/assertj/examples/SoftAssertionsExamples.java
+++ src/test/java/org/assertj/examples/SoftAssertionsExamples.java
@@ -17,25 +17,35 @@ import org.assertj.core.api.SoftAssertions;
 import org.assertj.examples.data.Mansion;
 import org.junit.Test;
 
+/**
+ * 数字的断言示例。
+ */
 public class SoftAssertionsExamples extends AbstractAssertionsExamples {
-
 
   @Test
   public void host_dinner_party_where_nobody_dies() {
     Mansion mansion = new Mansion();
     mansion.hostPotentiallyMurderousDinnerParty();
     SoftAssertions softly = new SoftAssertions();
-    softly.assertThat(mansion.guests()).as("Living Guests").isEqualTo(7);
-    softly.assertThat(mansion.kitchen()).as("Kitchen").isEqualTo("clean");
-    softly.assertThat(mansion.library()).as("Library").isEqualTo("clean");
-    softly.assertThat(mansion.revolverAmmo()).as("Revolver Ammo").isEqualTo(6);
-    softly.assertThat(mansion.candlestick()).as("Candlestick").isEqualTo("pristine");
-    softly.assertThat(mansion.colonel()).as("Colonel").isEqualTo("well kempt");
-    softly.assertThat(mansion.professor()).as("Professor").isEqualTo("well kempt");
+    softly.assertThat(mansion.guests())
+        .as("Living Guests").isEqualTo(7);
+    softly.assertThat(mansion.kitchen())
+        .as("Kitchen").isEqualTo("clean");
+    softly.assertThat(mansion.library())
+        .as("Library").isEqualTo("clean");
+    softly.assertThat(mansion.revolverAmmo())
+        .as("Revolver Ammo").isEqualTo(6);
+    softly.assertThat(mansion.candlestick())
+        .as("Candlestick").isEqualTo("pristine");
+    softly.assertThat(mansion.colonel())
+        .as("Colonel").isEqualTo("well kempt");
+    softly.assertThat(mansion.professor())
+        .as("Professor").isEqualTo("well kempt");
     try {
       softly.assertAll();
     } catch (SoftAssertionError e) {
       logAssertionErrorMessage("SoftAssertion errors example", e);
     }
   }
+
 }
