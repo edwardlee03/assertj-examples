dc09d44c1aab34f4a8377ff0c52a597bdc472620
edwardlee03 <bert825_work@163.com>
2022-03-19 23:33:03 +0800

    improve the code and comment of NumberAssertionsExamples

--- src/test/java/org/assertj/examples/NumberAssertionsExamples.java
+++ src/test/java/org/assertj/examples/NumberAssertionsExamples.java
@@ -21,24 +21,30 @@ import java.util.Comparator;
 import org.assertj.examples.comparator.AbsValueComparator;
 import org.junit.Test;
 
-
 /**
  * Number assertions examples.<br>
+ * 数字的断言示例。
  * 
  * @author Joel Costigliola
  */
 public class NumberAssertionsExamples extends AbstractAssertionsExamples {
 
   @Test
-  public void number_assertions_examples() throws Exception {
+  public void number_assertions_examples() {
 
     // equals / no equals assertions
-    assertThat(sam.age).isEqualTo(38);
-    assertThat(frodo.age).isEqualTo(33).isNotEqualTo(sam.age);
+    assertThat(sam.age)
+        .isEqualTo(38);
+    assertThat(frodo.age)
+        .isEqualTo(33).isNotEqualTo(sam.age);
 
     // <= < > >= assertions
-    assertThat(sam.age).isGreaterThan(frodo.age).isGreaterThanOrEqualTo(38);
-    assertThat(frodo.age).isLessThan(sam.age).isLessThanOrEqualTo(33);
+    assertThat(sam.age)
+        .isGreaterThan(frodo.age)
+        .isGreaterThanOrEqualTo(38);
+    assertThat(frodo.age)
+        .isLessThan(sam.age)
+        .isLessThanOrEqualTo(33);
     assertThat(sam.age).isBetween(frodo.age, gimli.age);
 
     // shortcuts for assertions : > 0, < 0 and == 0
@@ -56,14 +62,23 @@ public class NumberAssertionsExamples extends AbstractAssertionsExamples {
   public void number_assertions_with_custom_comparison_examples() {
 
     // with absolute values comparator : |-8| == |8|
-    assertThat(-8).usingComparator(absValueComparator).isEqualTo(8);
-    assertThat(-8.0).usingComparator(new AbsValueComparator<Double>()).isEqualTo(8.0);
-    assertThat((byte) -8).usingComparator(new AbsValueComparator<Byte>()).isEqualTo((byte) 8);
-    assertThat(new BigDecimal("-8")).usingComparator(new AbsValueComparator<BigDecimal>()).isEqualTo(
-        new BigDecimal("8"));
+    assertThat(-8)
+        .usingComparator(absValueComparator)
+        .isEqualTo(8);
+    assertThat(-8.0)
+        .usingComparator(new AbsValueComparator<Double>())
+        .isEqualTo(8.0);
+    assertThat((byte) -8)
+        .usingComparator(new AbsValueComparator<Byte>())
+        .isEqualTo((byte) 8);
+    assertThat(new BigDecimal("-8"))
+        .usingComparator(new AbsValueComparator<BigDecimal>())
+        .isEqualTo(new BigDecimal("8"));
 
     // works with arrays !
-    assertThat(new int[] { -1, 2, 3 }).usingElementComparator(absValueComparator).contains(1, 2, -3);
+    assertThat(new int[] { -1, 2, 3 })
+        .usingElementComparator(absValueComparator)
+        .contains(1, 2, -3);
   }
 
 
@@ -85,7 +100,6 @@ public class NumberAssertionsExamples extends AbstractAssertionsExamples {
       // this message is formatted by JUnit to show what is different (looks nice in IDE but not so in the error
       // message)
       assertThat(e).hasMessage("expected:<42.0[]> but was:<42.0[f]>");
-      return;
     }
   }
 
@@ -93,12 +107,15 @@ public class NumberAssertionsExamples extends AbstractAssertionsExamples {
   public void big_decimals_assertions_examples() {
 
     // You can use String directly and we will create the corresponding BigDecimal for you, thus ...  
-    assertThat(new BigDecimal("8.0")).isEqualTo("8.0");
+    assertThat(new BigDecimal("8.0"))
+        .isEqualTo("8.0");
     // ... is equivalent to :
-    assertThat(new BigDecimal("8.0")).isEqualTo(new BigDecimal("8.0"));
+    assertThat(new BigDecimal("8.0"))
+        .isEqualTo(new BigDecimal("8.0"));
     
     // With BigDecimal, 8.0 is not equals to 8.00 but it is if you use compareTo()
-    assertThat(new BigDecimal("8.0")).isEqualByComparingTo(new BigDecimal("8.00"));
+    assertThat(new BigDecimal("8.0"))
+        .isEqualByComparingTo(new BigDecimal("8.00"));
 
     // The following won't work because it relies on equals methos
     // assertThat(new BigDecimal("8.0")).isGreaterThanOrEqualTo(new BigDecimal("8.00"));
@@ -108,15 +125,18 @@ public class NumberAssertionsExamples extends AbstractAssertionsExamples {
         return bigDecimal1.compareTo(bigDecimal2);
       }
     };
-    assertThat(new BigDecimal("8.0")).usingComparator(bigDecimalComparator)
+    assertThat(new BigDecimal("8.0"))
+        .usingComparator(bigDecimalComparator)
         .isEqualTo(new BigDecimal("8.00"))
         .isGreaterThanOrEqualTo(new BigDecimal("8.00"));
   }
 
   @Test
   public void number_assertions_with_offset_examples() {
-    assertThat(8.1).isEqualTo(8.0, offset(0.1));
-    assertThat(8.2f).isEqualTo(8.0f, offset(0.2f));
+    assertThat(8.1)
+        .isEqualTo(8.0, offset(0.1));
+    assertThat(8.2f)
+        .isEqualTo(8.0f, offset(0.2f));
     try {
       assertThat(8.1f).isEqualTo(8.0f, offset(0.1f));
     } catch (AssertionError e) {
@@ -125,7 +145,3 @@ public class NumberAssertionsExamples extends AbstractAssertionsExamples {
   }
 
 }
-
-
-
-
